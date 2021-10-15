ddce4b6011d4cb629055733fa62fa69103045b84
Vrukshav <84898245+Vrukshav@users.noreply.github.com>
2021-10-15 12:31:18 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,182 @@
+onEvent("button2", "click", function( ) {
+  open("https://editor.p5js.org/vvrukshav/present/cbi9xnmcB");
+  
+});
+onEvent("button3", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/jum-0YIIrRF6bQFd5RpkjsQxyBg1cnLxS-nVk_05gmQ");
+  
+});
+onEvent("button13", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/9odH-wrZbqUeG-6sk6luw8HDlAt1pd86NshupZMC4Nk");
+  
+});
+onEvent("button4", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/xXu1uWbli1ozLePDZcTIbTbSnSMopdbQ7OBaRAF3Ts4");
+  
+});
+onEvent("button12", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/5hB7LqOZW_HsbK_Z_3LAZi4fbFGduHEHHCZf_nCQJjs");
+  
+});
+onEvent("button5", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/yL2vX3JXEl9sM-fu7DceF2fLRF0oJWll-da-SUBi5V4");
+  
+});
+onEvent("button11", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/HX2zuLjUfW8xEG2HpWfevg4RHlM1wbfs7cxl8nUpZqU");
+  
+});
+onEvent("button6", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/kxzO40sD_pmtbZBNhaPMIVXNIjoERolqPGDH3UHpp_w");
+  
+});
+onEvent("button10", "click", function( ) {
+  open("https://editor.p5js.org/vvrukshav/present/ZKv6zxwXg");
+  
+});
+onEvent("button7", "click", function( ) {
+  open("https://editor.p5js.org/vvrukshav/present/lojAfmq4f");
+  
+});
+onEvent("button9", "click", function( ) {
+  open("https://editor.p5js.org/vvrukshav/present/tbV2Yb-1N");
+  
+});
+onEvent("button8", "click", function( ) {
+  open("https://editor.p5js.org/vvrukshav/present/Sa3wFR9F-");
+  
+});
+onEvent("button14", "click", function( ) {
+  setScreen("screen2");
+  
+  
+});
+onEvent("button16", "click", function( ) {
+  open("https://preview.p5js.org/vvrukshav/present/NgWGOAP1t");
+  
+});
+onEvent("button17", "click", function( ) {
+  open("https://preview.p5js.org/vvrukshav/present/Uzx9PFExl");
+  
+});
+onEvent("button27", "click", function( ) {
+  open("https://preview.p5js.org/vvrukshav/present/fwzb_0Mdv");
+  
+});
+onEvent("button18", "click", function( ) {
+  open("https://preview.p5js.org/vvrukshav/present/7rbLUqa7S");
+  
+});
+onEvent("button26", "click", function( ) {
+  open("https://vrukshav.github.io/Collecting-Treasure/");
+  
+});
+onEvent("button19", "click", function( ) {
+  open("https://preview.p5js.org/vvrukshav/present/Uzx9PFExl");
+  
+});
+onEvent("button25", "click", function( ) {
+  open("https://vrukshav.github.io/Capstone-Project-Class-19-Rocket-To-The-Moon-/");
+  
+});
+onEvent("button20", "click", function( ) {
+  open("https://preview.p5js.org/vvrukshav/present/yaGXVViaO");
+  
+});
+onEvent("button24", "click", function( ) {
+  open("https://vrukshav.github.io/Project-21-Ball-To-The-Dustbin/");
+  
+});
+onEvent("button21", "click", function( ) {
+  open("https://vrukshav.github.io/Project-PRO-C22-Newton-s-Cradle/");
+  
+});
+onEvent("button23", "click", function( ) {
+  open("https://vrukshav.github.io/Project-Pro-C23-/");
+  
+});
+onEvent("button22", "click", function( ) {
+  open("https://vrukshav.github.io/Project-PRO-C24-/");
+  
+});
+onEvent("button30", "click", function( ) {
+  open("https://vrukshav.github.io/Project-25-EPIC-ARCHERY-STAGE-3/");
+  
+});
+onEvent("button31", "click", function( ) {
+  open("https://vrukshav.github.io/Project-26---EPIC-ARCHERY-STAGE-4/");
+  
+});
+onEvent("button41", "click", function( ) {
+  open("https://vrukshav.github.io/Project-27---EPIC-ARCHERY-STAGE-5/");
+  
+});
+onEvent("button32", "click", function( ) {
+  open("https://vrukshav.github.io/Project-28---EPIC-ARCHERY-STAGE-5/");
+  
+});
+onEvent("button40", "click", function( ) {
+  open("https://vrukshav.github.io/Project-29--Zombie-Crush-1/");
+  
+});
+onEvent("button33", "click", function( ) {
+  open("https://vrukshav.github.io/Project-30---Zombie-Crush-2/");
+  
+});
+onEvent("button39", "click", function( ) {
+  open("https://vrukshav.github.io/Project-31---Crush-The-Zombies---3/");
+  
+});
+onEvent("button34", "click", function( ) {
+  open("https://vrukshav.github.io/Blower-Game/");
+  
+});
+onEvent("button38", "click", function( ) {
+  open("https://vrukshav.github.io/Feed-The-Bunny-Bubble-/");
+  
+});
+onEvent("button35", "click", function( ) {
+  open("https://vrukshav.github.io/Capstone-Class-34-Decide-your-fate-in-a-Visual-way-/");
+  
+});
+onEvent("button37", "click", function( ) {
+  open("https://vrukshav.github.io/Project-35/");
+  
+});
+onEvent("button36", "click", function( ) {
+  open("https://vrukshav.github.io/My-Quiz-Form/");
+  
+});
+onEvent("button44", "click", function( ) {
+  open("https://vrukshav.github.io/MyQuiz-Game/");
+  
+});
+onEvent("button45", "click", function( ) {
+  open("https://vrukshav.github.io/Project-PRO-38/");
+  
+});
+onEvent("button55", "click", function( ) {
+  open("https://vrukshav.github.io/Project-39/");
+  
+});
+onEvent("button46", "click", function( ) {
+  open("https://vrukshav.github.io/Project-40/");
+  
+});
+onEvent("button54", "click", function( ) {
+  open("https://vrukshav.github.io/Project-C41---Fruit-Catcher-----2/");
+  
+});
+onEvent("button47", "click", function( ) {
+  open("https://vrukshav.github.io/PRO-C42-SHOOTING-RANGE/");
+  
+});
+onEvent("button42", "click", function( ) {
+  setScreen("screen4"); 
+  
+});
+onEvent("button28", "click", function( ) {
+ setScreen("screen3"); 
+  
+});
+
